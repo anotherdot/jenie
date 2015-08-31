03304012f2f6645660adeaaeca2e6140966702f5
lukafaxx <lukafaxx@gmail.com>
2015-08-31 22:52:45 +0200

    enhance structure

--- import/global.js
+++ import/global.js
@@ -1,15 +1,12 @@
   $(document).ready(function() {
 
-    // ----- Toggle App Info
+    // ----- Toggle Faq Info
     $('#toggle-faq').on('click', function(){
-      $('.faq-content').toggle('fast');
-      var el = $(this);
-      if (el.text() == el.data('text-swap')) {
-        el.text(el.data('text-original'));
-      } else {
-        el.data('text-original', el.text());
-        el.text(el.data('text-swap'));
-      }
+      $('.faq-content').slideToggle('400');
+    });
+
+    $('#to-toggle-faq').on('click', function(){
+      $('.faq-content').slideDown('400');
     });
 
   });
